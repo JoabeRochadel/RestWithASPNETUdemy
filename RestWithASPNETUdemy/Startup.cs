18db0cf9ec12c3b0cd18aceeba21c977690c024a
Joabe Rochadel <joaberochadel@gmail.com>
2022-05-25 22:37:11 -0300

    Add project files.

---
+++ RestWithASPNETUdemy/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Services.Implementations;
+
+namespace RestWithASPNETUdemy
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add business to the container.
+        public void ConfigureServices(IServiceCollection business)
+        {
+
+            business.AddControllers();
+
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+
+            business.AddDbContext<MySQLContext>(options => options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
+
+            business.AddApiVersioning();
+
+            // Injecao de dependencia
+            business.AddScoped<IPersonRepository, PersonBusinessImplementation>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
